29e3cec8a59c8f5fcd9709043c19e3401cd9bd9b
stefan-scott <scottst@spsd.sk.ca>
2026-05-11 10:04:59 -0600

    done

--- 15 - L Systems.py
+++ 15 - L Systems.py
@@ -1,64 +1,39 @@
-# L - Systems Demo
-# Mr. Scott
+# L-Systems
 # May 8, 2026
+# Mr. Scott
 
-# Set Up Turtle Boilerplate - TODO
+# Turtle BoilerPlate
 import turtle
 t = turtle.Turtle()
 wn = turtle.Screen()
 wn.setup(800,800)
 t.speed(0)
 
-def draw_L_system(instructions, angle, distance):
-    # have the turtle make a draw based
-    # on the Lsystem instructions
-    for cmd in instructions:
-        if cmd == "F":
-            t.fd(distance)
-        elif cmd == "B":
-            t.bk(distance)
-        elif cmd == "+":
-            t.right(angle)
-        elif cmd == "-":
-            t.left(angle)
 
-# Create the L-System
+# Create L-System Functions
 def apply_rules(ch):
-    # inspect ch(character) and apply the rules
-    # to it (if any).
-    if ch == "X":
-        return "YF+XF+Y"
-    elif ch == "Y":
-        return "XF-YF-X"
+    # Apply the rules to a single char
+    # -- need to update for each Lsystem
+    if ch == "A":
+        return "B"
+    elif ch == "B":
+        return "AB"
     else:
         return ch
-
-def process_string(original_str): #ABBAB 
-    # Loop through original_str and apply rules
-    # to each character individually
+    
+def process_string(original_str): #"ABBAB"
+    # loop through our original_str and apply
+    # the rules to each character
     next_str = ""
     for c in original_str:
         next_str = next_str + apply_rules(c)
     return next_str
 
 def create_L_system(num_steps, axiom):
-    #Run L-System for a set number of growth steps
-    curr_str = axiom
+    current_str = axiom
     for i in range(num_steps):
-        next_str = process_string(curr_str)
-        curr_str = next_str
-    return curr_str
+        next_str = process_string(current_str)
+        current_str = next_str
+    return current_str
 
-# Main Code Section
-commands = create_L_system(9, "FX")
-print(commands)
-
-turtle.tracer(False)
-t.up()
-t.goto(-200,200)
-t.down()
-
-draw_L_system(commands, 60, 1)
-
-
-turtle.exitonclick()
+# Main Code
