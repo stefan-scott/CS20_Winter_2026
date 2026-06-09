89b01060292b8993d74ecec6ea81d111c369410f
stefan-scott <scottst@spsd.sk.ca>
2026-06-09 09:28:43 -0600

    Update 26 Images and Conditionals.py

--- 26 Images and Conditionals.py
+++ 26 Images and Conditionals.py
@@ -8,7 +8,7 @@
 # image boilerplate
 import image
 
-img = image.Image("moon.jpg")
+img = image.Image("sneakers.jpg")
 width = img.get_width()
 height = img.get_height()
 wn = image.ImageWin(width,height)
@@ -19,16 +19,31 @@ def change_sky(p):
     # if so, change to a new color (blue)
     # input p (Pixel), output Pixel Object
     total = p.get_red() + p.get_green() + p.get_blue()
-    if total == 0: #sky pixel
+    if total < 40: #sky pixel
         return image.Pixel(95, 146, 227)
     else:
         return p
+
+
+def left_half_grey(p, x):
+    # change left half of image to greyscale (blue channel)
+    # input: p → Pixel object   x: horizontal location
+    # 
 # main program
 
 
 
+
+# Program 1: Remove Sky
+# for x in range(width):
+#     for y in range(height):
+#         src_pixel = img.get_pixel(x,y)
+#         new_pixel = change_sky(src_pixel)
+#         img.set_pixel(x,y,new_pixel)
+#     img.draw(wn)
+
 # p = img.get_pixel(40,40)
 # print(p.get_red(),p.get_green(),p.get_blue())
 
-img.draw(wn)
+
 wn.exitonclick() 
