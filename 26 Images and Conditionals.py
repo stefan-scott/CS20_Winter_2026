73b34a328a160f2e12a13d80122f4f781067105c
stefan-scott <scottst@spsd.sk.ca>
2026-06-10 09:06:19 -0600

    Update 26 Images and Conditionals.py

--- 26 Images and Conditionals.py
+++ 26 Images and Conditionals.py
@@ -8,7 +8,9 @@
 # image boilerplate
 import image
 
-img = image.Image("sneakers.jpg")
+img = image.Image("rooster.jpg")
+smile = image.Image("smile.png")
+
 width = img.get_width()
 height = img.get_height()
 wn = image.ImageWin(width,height)
@@ -28,9 +30,39 @@ def change_sky(p):
 def left_half_grey(p, x):
     # change left half of image to greyscale (blue channel)
     # input: p → Pixel object   x: horizontal location
-    # 
+    # output: Pixel object
+    if x < width/2:
+        b = p.get_blue()
+        return image.Pixel(b,b,b)
+    else:
+        return p
+    
 # main program
+# Challenge 3 - joining two images
+s_width = smile.get_width()
+s_height = smile.get_height()
+for x in range(s_width):
+    for y in range(s_height):
+        src_pixel = smile.get_pixel(x,y)
+        total = src_pixel.get_red()+src_pixel.get_green()+src_pixel.get_blue()
+        
+        if total < 750:
+            dest_x = x + 150
+            dest_y = y + 50
+            img.set_pixel(dest_x,dest_y,src_pixel)
+        
+    img.draw(wn)
 
+
+
+# Challenge 2: change left half only
+# for y in range(height):
+#     for x in range(width):
+#         src_pixel = img.get_pixel(x,y)
+#         new_pixel = left_half_grey(src_pixel, x)
+#         img.set_pixel(x,y,new_pixel)
+#     img.draw(wn)
+            
 
 
 
